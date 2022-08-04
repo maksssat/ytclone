59e807b78cae2f6549dd1d8e0c063a27ffe244b9
Maksat Sagandykov <54390231+maksssat@users.noreply.github.com>
2022-08-04 19:25:13 +0600

    добавлены aria атрибуты в компонент header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,34 +14,34 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.left}>
-        <button className={styles.button} type="button">
+        <button className={styles.button} type="button" aria-label="Меню" aria-controls="menu">
           <MenuIcon className={styles.button_icon} aria-hidden="true" />
         </button>
-        <LogoIcon className={styles.logo} />
+        <LogoIcon className={styles.logo} aria-label="Логотип"/>
         <span className={`${styles.country_code}`}>KZ</span>
       </div>
       <div className={styles.center}>
-        <form className={styles.form}>
+        <form className={styles.form} aria-label="Поиск видео">
           <div className={styles.search_input_container}>
             <input className={styles.search_input} type="text" placeholder="Введите запрос" />
             <KeyboardIcon className={styles.keyboard_icon} />
           </div>
         </form>
-        <button className={styles.search_button} type="button">
+        <button className={styles.search_button} type="button" aria-label="Искать">
           <SearchIcon className={styles.button_icon} aria-hidden="true" />
         </button>
-        <button className={styles.voice_input_button} type="button">
+        <button className={styles.voice_input_button} type="button" aria-label="Голосовой поиск">
           <VoiceInputIcon className={styles.button_icon} aria-hidden="true" />
         </button>
       </div>
       <div className={styles.right}>
-        <button className={styles.button} type="button">
+        <button className={styles.button} type="button" aria-label="Добавить видео">
           <VideoIcon className={styles.button_icon} aria-hidden="true" />
         </button>
-        <button className={styles.button} type="button">
+        <button className={styles.button} type="button" aria-label="Уведомления">
           <NotificationsIcon className={styles.button_icon} aria-hidden="true" />
         </button>
-        <button className={styles.button} type="button">
+        <button className={styles.button} type="button" aria-label="Аккаунт">
           <PersonIcon className={styles.button_icon} aria-hidden="true" />
         </button>
       </div>
