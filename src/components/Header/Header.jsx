e50e8bd5e1fcf0ffd3390544c2a3e8e888afed14
Maksat Sagandykov <54390231+maksssat@users.noreply.github.com>
2022-08-11 17:04:55 +0600

    созданы окна меню уведомлений и создания видео

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,13 +9,27 @@ import { ReactComponent as VideoIcon } from "./images/video.svg";
 import { ReactComponent as NotificationsIcon } from "./images/notifications.svg";
 import { ReactComponent as PersonIcon } from "./images/person.svg";
 import { ReactComponent as VoiceInputIcon } from "./images/voice-input.svg";
+import { ReactComponent as StartStreamIcon } from "./images/stream.svg";
+import { ReactComponent as AddVideoIcon } from "./images/your_videos.svg";
+import { ReactComponent as SettingsIcon } from "./images/settings.svg";
 import HeaderButton from "../HeaderButton/HeaderButton";
+import MenuLink from "../MenuLink/MenuLink";
 
 export default function Header() {
   const [menuIsOpen, setMenuIsOpen] = useState(true);
+  const [addVideoMenuIsOpen, setAddVideoMenuIsOpen] = useState(false);
+  const [notificationsMenuIsOpen, setNotificationsMenuIsOpen] = useState(false);
 
   function handleMenuButtonClick() {
     setMenuIsOpen(!menuIsOpen);
+  }
+
+  function handleAddVideoButtonClick() {
+    setAddVideoMenuIsOpen(!addVideoMenuIsOpen);
+  }
+
+  function handleNotificationsButtonClick() {
+    setNotificationsMenuIsOpen(!notificationsMenuIsOpen);
   }
 
   return (
@@ -32,8 +46,37 @@ export default function Header() {
       </div>
 
       <div className={styles.right}>
-        <HeaderButton icon={VideoIcon} label="Добавить видео" onClick={handleMenuButtonClick} />
-        <HeaderButton icon={NotificationsIcon} label="Уведомления" />
+        <HeaderButton icon={VideoIcon} label="Добавить видео" onClick={handleAddVideoButtonClick} />
+
+        {addVideoMenuIsOpen === true ? (
+          <ul className={styles.add_video_menu}>
+            <MenuLink icon={AddVideoIcon} text="Добавить видео" shrinked={false} />
+            <MenuLink icon={StartStreamIcon} text="Начать трансляцию" shrinked={false} />
+          </ul>
+        ) : null}
+
+        <HeaderButton
+          icon={NotificationsIcon}
+          label="Уведомления"
+          onClick={handleNotificationsButtonClick}
+        />
+
+        {notificationsMenuIsOpen === true ? (
+          <div className={styles.notifications_menu} aria-label="Список уведомлений">
+            <div className={styles.notifications_menu_header}>
+              Уведомления <SettingsIcon className={styles.notifications_menu_header_icon} />
+            </div>
+            <div className={styles.notifications_menu_list}>
+              <NotificationsIcon className={styles.notifications_menu_list_icon} />
+              <p>Здесь пока ничего нет</p>
+              <p>
+                В этом разделе будут собраны уведомления о новых видео на каналах, на которые вы
+                подписаны.
+              </p>
+            </div>
+          </div>
+        ) : null}
+
         <HeaderButton icon={PersonIcon} label="Аккаунт" />
       </div>
       <Menu isOpen={menuIsOpen} />
